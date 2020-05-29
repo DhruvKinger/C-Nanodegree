3eadfc19cd8c568a751ca6987fd9c65e9791c409
Dhruv Kinger <cse1785@outlook.com>
2020-05-29 08:49:22 +0530

    Update system.cpp

--- system.cpp
+++ system.cpp
@@ -13,7 +13,7 @@ using std::size_t;
 using std::string;
 using std::vector;
 
-// TODO: Return the system's CPU
+
 Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
@@ -25,8 +25,8 @@ std::string System::Kernel() { return string(); }
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+//done
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
@@ -34,5 +34,7 @@ int System::RunningProcesses() { return 0; }
 // TODO: Return the total number of processes on the system
 int System::TotalProcesses() { return 0; }
 
-// TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return 0; }
+// done
+long int System::UpTime() {
+  return LinuxParser::UpTime();
+                          }
