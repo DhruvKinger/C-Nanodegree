dbf13d64df0e9d450dd3f117166de74163955c1b
Dhruv Kinger <cse1785@outlook.com>
2020-05-29 08:51:33 +0530

    Update system.cpp

--- system.cpp
+++ system.cpp
@@ -19,17 +19,17 @@ Processor& System::Cpu() { return cpu_; }
 // TODO: Return a container composed of the system's processes
 vector<Process>& System::Processes() { return processes_; }
 
-// TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+// done
+std::string System::Kernel() { return  LinuxParser::Kernel(); }
 
-// TODO: Return the system's memory utilization
-float System::MemoryUtilization() { return 0.0; }
+// done
+float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
 //done
 std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
+int System::RunningProcesses() {  return LinuxParser::RunningProcesses(); }
 
 // TODO: Return the total number of processes on the system
 int System::TotalProcesses() { return 0; }
